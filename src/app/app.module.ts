8bd8760e45acf6134a00f5f406f07c05d18ba844
Scott A. Hyslop <sahyslop@gmail.com>
2021-11-30 18:02:20 -0500

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { AlertWarningComponent } from './alert-warning/alert-warning.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertWarningComponent,
+    AlertSuccessComponent
   ],
   imports: [
     BrowserModule,
