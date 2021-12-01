ddbe83505c6cd14df742963f03bfde0b4d13dff1
Scott A. Hyslop <sahyslop@gmail.com>
2021-11-30 21:41:55 -0500

    Data binding added, intro written for app.component.html

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!!";
+  serverName='';
 
-  constructor() { }
+  constructor() { 
+
+    setTimeout(() => { this.allowNewServer=true;},2000);
+  }
 
   ngOnInit(): void {
   }
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!  Its name is: ' + this.serverName;
+  }
 
+  onUpdateServerName(event:any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
